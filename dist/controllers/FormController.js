0c46d3d098ab72b0160e8e559f020b04da33adce
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-20 15:38:37 +0700

    fix

--- dist/controllers/FormController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class FormController {
-    async create(req, res) {
-        const { body: payload } = req;
-        console.log(payload);
-    }
-}
-exports.default = new FormController;
