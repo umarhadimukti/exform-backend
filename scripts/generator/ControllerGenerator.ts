aa26ac8d10c7b62647461ea07dda03dd0d6c2bd4
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-27 23:35:55 +0700

    add comment

--- scripts/generator/ControllerGenerator.ts
+++ scripts/generator/ControllerGenerator.ts
@@ -1,7 +1,17 @@
 
 class ControllerGenerator
 {
-    // define
+    // define controller dir and template path
+
+    // define ensure directory is exists
+
+    // define generator method
+        // call ensure directory exists
+        // make controller path
+        // check if exists controller path
+        // call generate controller template
+        // write file in new path of controller
+        // print success
 
 
 }
