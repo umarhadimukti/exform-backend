86d0aed2247e7869eb29b9130c3b8f64797d41aa
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-27 20:24:01 +0700

    add comment

--- src/libs/validateEmail.ts
+++ src/libs/validateEmail.ts
@@ -4,12 +4,14 @@ import { PayloadQuestionAnswers } from "../types/payloadType";
 
 export const validateEmail = (form: FormWithQuestions, payload: PayloadQuestionAnswers[]) =>
 {
-
     const validEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
     const filteredQuestions = form.questions.filter((question: Question) => {
         if (question.type === 'email') {
+            // search answer that's belong to the question
             const answer = payload.find((data: any) => data.question_id === question.id);
             if (answer) {
+                // if email is matches with regex, return true
                 if (!answer.answer.match(validEmailRegex)) {
                     return true;
                 }
