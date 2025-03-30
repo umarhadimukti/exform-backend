3825656f1fe61b19d75eb981d6d093e374d90a6a
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-30 12:36:44 +0700

    index response controller

--- src/controllers/ResponseController.ts
+++ src/controllers/ResponseController.ts
@@ -2,16 +2,42 @@ import { Request, Response } from 'express';
 import { BaseController } from '../interfaces/ControllerInterface';
 import { ZodError } from "zod";
 import CustomError from '../libs/errors/CustomError';
+import { Answer, Form } from '@prisma/client';
+import { prisma } from '../db/connection';
 
 class ResponseController extends BaseController {
     public async index(req: Request, res: Response): Promise<Response> {
         try {
-            
+           const { user } = req;
+            const { formId } = req.params;
+
+            const parsedFormId: number = parseInt(formId, 10);
+            if (!parsedFormId || isNaN(parsedFormId)) throw new CustomError('invalid form id.', 400);
+
+            const isUserForm: Form | null = await prisma.form.findFirst({
+                where: { user_id: user?.id, id: parsedFormId }
+            });
+            if (!isUserForm) throw new CustomError('invalid form (you don\'t have access with this form.', 400);
+
+            const answers = await prisma.answer.findMany({
+                where: { form_id: isUserForm?.id },
+                select: {
+                    id: true,
+                    user_id: true,
+                    form_id: true,
+                    question: {
+                        select: { question: true },
+                    },
+                    value: true,
+                },
+                distinct: ['question_id'],
+                orderBy: { created_at: 'desc' },
+            });
 
             return res.status(200).json({
-                status: true,
-                length: 0,
-                data: []
+                status: false,
+                length: answers?.length,
+                answers,
             });
         } catch (error) {
             return this.handleError(res, error, 'failed to get data');
