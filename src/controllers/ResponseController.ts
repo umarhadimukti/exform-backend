aae3d6b0d33c970403a209b85df0b3a82a355972
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-03-31 19:31:29 +0700

    remove unused function

--- src/controllers/ResponseController.ts
+++ src/controllers/ResponseController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { BaseController } from '../interfaces/ControllerInterface';
 import { ZodError } from "zod";
 import CustomError from '../libs/errors/CustomError';
-import { Answer, Form } from '@prisma/client';
+import { Form } from '@prisma/client';
 import { prisma } from '../db/connection';
 
 class ResponseController extends BaseController {
@@ -41,62 +41,6 @@ class ResponseController extends BaseController {
             });
         } catch (error) {
             return this.handleError(res, error, 'failed to get data');
-        }
-    }
-
-    public show(req: Request, res: Response): Response {
-        try {
-            // logic here..
-
-            return res.status(200).json({
-                status: true,
-                data: {}
-            });
-        } catch (error) {
-            return this.handleError(res, error, 'failed to get data');
-        }
-    }
-
-    public create(req: Request, res: Response): Response {
-        try {
-            // logic here..
-
-            return res.status(201).json({
-                status: true,
-                message: 'data successfully created.',
-                data: req.body
-            });
-        } catch (error) {
-            return this.handleError(res, error, 'failed to create data');
-        }
-    }
-
-    public update(req: Request, res: Response): Response {
-        try {
-            const { id } = req.params;
-            // logic here..
-
-            return res.status(200).json({
-                status: true,
-                message: 'data successfully updated.',
-                data: { id, ...req.body }
-            });
-        } catch (error) {
-            return this.handleError(res, error, 'failed to update data');
-        }
-    }
-
-    public delete(req: Request, res: Response): Response {
-        try {
-            const { id } = req.params;
-            // logic here..
-
-            return res.status(200).json({
-                message: 'data successfully deleted.',
-                data: { id }
-            });
-        } catch (error) {
-            return this.handleError(res, error, 'failed to delete data');
         }
     }
 
