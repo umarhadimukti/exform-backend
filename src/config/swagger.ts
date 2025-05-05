2f8a4b143c0aed0590a1e775a656790fd378187c
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-05 12:54:05 +0700

    define configuration of swagger doc

---
+++ src/config/swagger.ts
@@ -0,0 +1,68 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API for Form Application',
+        version: '1.0.0',
+        description: 'This is a simple CRUD API application made with Express and documented with Swagger',
+        contact: {
+            name: 'Exform: Form Application',
+            url: 'localhost:3000',
+            email: 'umarhadimukti@gmail.com',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3002',
+                description: 'Development server',
+            },
+        ],
+    },
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+                description: 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer {token}"',
+            },
+        },
+    },
+    security: [
+        {
+            bearerAuth: [],
+        },
+    ],
+    tags: [
+        {
+            name: 'auth',
+            description: 'Authentication routes',
+        },
+        {
+            name: 'form',
+            description: 'Form routes',
+        },
+        {
+            name: 'question',
+            description: 'Question routes',
+        },
+        {
+            name: 'option',
+            description: 'Option routes',
+        },
+        {
+            name: 'user',
+            description: 'User routes',
+        },
+        {
+            name: 'seed',
+            description: 'Seed routes',
+        },
+    ],
+    paths: {},
+  },
+  apis: ['./src/routes/*.ts'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+export default swaggerDocs;
